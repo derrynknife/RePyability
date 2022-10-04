206da2a34d1eb04c7006e74f911d2c13f3b75aa9
derrynknife <derryn.knife@gmail.com>
2022-10-05 06:36:12 +1000

    changed optimal replacement finding to be able to do non-parametric

--- repyability/__init__.py
+++ repyability/__init__.py
@@ -1,24 +1,9 @@
 import numpy as np
+from surpyval.parametric import Parametric
+from surpyval.nonparametric import NonParametric
+
 from scipy.integrate import quad
 from scipy.optimize import minimize
-
-class Interference():
-    def __init__(self, stress, strength):
-        self.stress = stress
-        self.strength = strength
-
-    def reliability(self, n=1000):
-        if self.stress.dist.name == "Normal":
-            if self.strength.dist.name == "Normal":
-                print("Normals")
-                mu = self.strength.params[0] - self.stress.params[0]
-                sig = np.sqrt(self.strength.params[1]**2 + 
-                    self.stress.params[1]**2)
-                return 1 - self.stress.dist.ff(0, mu, sig)
-        stress_rvs = self.stress.random(n)
-        strength_rvs = self.strength.random(n)
-        return 1 - np.sum((stress_rvs > strength_rvs).astype(np.float64))/n
-
 
 class NonRepairable():
     """
@@ -26,6 +11,13 @@ class NonRepairable():
     """
     def __init__(self, distribution):
         self.dist = distribution
+        if type(distribution) == Parametric:
+            self.model_parameterization = 'parametric'
+        elif type(distribution) == NonParametric:
+            self.model_parameterization = 'non_parametric'
+        else:
+            raise ValueError("Unknown TTF Model")
+
         self.cost_rate = np.vectorize(self._cost_rate)
         self.cost_rate_single_cycle = np.vectorize(self._cost_rate_single_cycle)
 
@@ -36,6 +28,11 @@ class NonRepairable():
 
     def avg_replacement_time(self, t):
         out = quad(self.dist.sf, 0, t)
+        return out[0]
+
+    def avg_replacement_time_non_parametric(self, t, interp):
+        func = lambda x : self.dist.sf(x, interp=interp)
+        out = quad(func, 0, t, limit=200)
         return out[0]
 
     def _cost_rate(self, t):
@@ -56,14 +53,47 @@ class NonRepairable():
     def _log_cost_rate_single_cycle(self, t):
         return np.log(self._cost_rate_single_cycle(t))
 
-    def find_optimal_replacement(self):
-        if self.dist.dist.name == "Weibull":
-            if self.dist.params[1] <= 1:
-                return np.inf
-        init = self.dist.mean()
-        bounds = ((1e-8, None),)
-        res = minimize(self._log_cost_rate, init, bounds=bounds, tol=1e-10)
-        self.optimisation_results = res
+    def _cost_rate_non_parametric(self, t, interp):
+        R = self.dist.sf(t, interp=interp) 
+        planned_costs = R * self.cp
+        unplanned_costs = (1 - R) * self.cu
+
+        avg_repl_time = self.avg_replacement_time_non_parametric(t, interp)
+        return (planned_costs + unplanned_costs) / avg_repl_time
+
+    def _log_cost_rate_non_parametric(self, t, interp):
+        return np.log(self._cost_rate_non_parametric(t, interp=interp))
+
+    def find_optimal_replacement(self, interp=None):
+        if self.model_parameterization == 'parametric':
+            if self.dist.dist.name == "Weibull":
+                if self.dist.params[1] <= 1:
+                    return np.inf
+        
+            init = self.dist.mean()
+            bounds = ((1e-8, None),)
+            res = minimize(self._log_cost_rate, init, bounds=bounds, tol=1e-10)
+            self.optimisation_results = res
+            return res.x[0]
+        else:
+            if interp is None:
+                raise ValueError("When using Non-Parametric model must select interpolation method.")
+            elif interp == 'step':
+                x = self.dist.x
+                RUL = (np.diff(x, prepend=0) * self.dist.sf(x)).cumsum()
+                cput = (self.cp * self.dist.sf(x) + self.cu * self.dist.ff(x)) / RUL
+                self.cput = cput
+                return x[np.argmin(cput)]
+            elif interp == 'linear':
+                init = self.dist.x.mean()
+                bounds = ((self.dist.x.min(), self.dist.x.max()),)
+                func = lambda x : self._cost_rate_non_parametric(x, interp)
+                res = minimize(func, init, bounds=bounds, tol=1e-10)
+                self.optimisation_results = res
+                return res.x[0]
+            else:
+                raise ValueError("`interp` must be either 'step' or 'linear'")
+
         return res.x[0]
 
     def find_optimal_replacement_single_cycle(self):
