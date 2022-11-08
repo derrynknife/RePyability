83893a9f401160adfbf445f93eec59fa89141ed2
Anthony Carbone <anthonydavidcarbone@gmail.com>
2022-11-08 12:56:34 -0800

    Added koon get_min_paths() tests

--- repyability/tests/conftest.py
+++ repyability/tests/conftest.py
@@ -1,5 +1,5 @@
 """
-Contains the pytest fixtures used by many of the tests. They are all define
+Contains the pytest fixtures used by many of the tests. They all define
 and return RBDs.
 """
 
@@ -162,7 +162,25 @@ def rbd_repeated_component_composite() -> RBD:
 
 
 @pytest.fixture
-def rbd_koon1() -> RBD:
+def rbd_series_koon() -> RBD:
+    """
+    Series RBD w/ three intermediate nodes, the middle one with k=2.
+    i.e. this system doesn't work at all.
+    """
+    nodes = {1: "input_node", 2: 2, 3: 3, 4: 4, 5: "output_node"}
+    edges = [(1, 2), (2, 3), (3, 4), (4, 5)]
+    components = {
+        2: surv.Weibull.from_params([20, 2]),
+        3: surv.Weibull.from_params([100, 3]),
+        4: surv.Weibull.from_params([50, 20]),
+    }
+    k = {3: 2}
+    return RBD(nodes, components, edges, k)
+
+
+@pytest.fixture
+def rbd1_koon() -> RBD:
+    """Example 6.10 from Modarres & Kaminskiy, w/ valve k=2."""
     qp = 0.03
     qv = 0.01
     nodes = {
@@ -185,4 +203,95 @@ def rbd_koon1() -> RBD:
         "valve": FixedProbabilityFitter.from_params(1 - qv),
     }
     k = {"valve": 2}
+    return RBD(nodes, components, edges, k)
+
+
+@pytest.fixture
+def rbd2_koon() -> RBD:
+    """rbd2 but k of node 7 is =2."""
+    edges = [(1, 2), (2, 3), (2, 4), (4, 7), (3, 5), (5, 6), (6, 7), (7, 8)]
+    nodes = {
+        1: "input_node",
+        8: "output_node",
+        2: 2,
+        3: 3,
+        4: 4,
+        5: 5,
+        6: 6,
+        7: 7,
+    }
+    components = {
+        2: surv.Weibull.from_params([20, 2]),
+        3: surv.Weibull.from_params([100, 3]),
+        4: surv.Weibull.from_params([50, 20]),
+        5: surv.Weibull.from_params([15, 1.2]),
+        6: surv.Weibull.from_params([80, 10]),
+        7: [
+            surv.Weibull.from_params([5, 1.1]),
+            surv.Weibull.from_params([5, 1.1]),
+            surv.Weibull.from_params([5, 1.1]),
+            surv.Weibull.from_params([5, 1.1]),
+        ],
+    }
+    k = {7: 2}
+    return RBD(nodes, components, edges, k)
+
+
+def rbd3_nodes_edges_components():
+    nodes = {0: "input_node", 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: "output_node"}
+    edges = [
+        (0, 1),
+        (0, 3),
+        (1, 2),
+        (3, 4),
+        (1, 5),
+        (3, 5),
+        (5, 2),
+        (5, 4),
+        (2, 6),
+        (4, 6),
+    ]
+    components = {
+        1: FixedProbabilityFitter.from_params(0.95),
+        2: FixedProbabilityFitter.from_params(0.95),
+        3: FixedProbabilityFitter.from_params(0.95),
+        4: FixedProbabilityFitter.from_params(0.95),
+        5: FixedProbabilityFitter.from_params(0.95),
+    }
+    return nodes, edges, components
+
+
+@pytest.fixture
+def rbd3_koon1() -> RBD:
+    """
+    Fig. 16.1 from "UNIT 16 RELIABILITY EVALUATION OF COMPLEX SYSTEMS" by
+    ignou (https://egyankosh.ac.in/bitstream/123456789/35170/1/Unit-16.pdf).
+    w/ k(5)=2
+    """
+    nodes, edges, components = rbd3_nodes_edges_components()
+    k = {5: 2}
+    return RBD(nodes, components, edges, k)
+
+
+@pytest.fixture
+def rbd3_koon2() -> RBD:
+    """
+    Fig. 16.1 from "UNIT 16 RELIABILITY EVALUATION OF COMPLEX SYSTEMS" by
+    ignou (https://egyankosh.ac.in/bitstream/123456789/35170/1/Unit-16.pdf).
+    w/ k(2)=2
+    """
+    nodes, edges, components = rbd3_nodes_edges_components()
+    k = {2: 2}
+    return RBD(nodes, components, edges, k)
+
+
+@pytest.fixture
+def rbd3_koon3() -> RBD:
+    """
+    Fig. 16.1 from "UNIT 16 RELIABILITY EVALUATION OF COMPLEX SYSTEMS" by
+    ignou (https://egyankosh.ac.in/bitstream/123456789/35170/1/Unit-16.pdf).
+    w/ k(2)=k(5)=2
+    """
+    nodes, edges, components = rbd3_nodes_edges_components()
+    k = {2: 2, 5: 2}
     return RBD(nodes, components, edges, k)
--- repyability/tests/test_rbd_koon.py
+++ repyability/tests/test_rbd_koon.py
@@ -15,9 +15,46 @@ def test_rbd_koon_default_k(rbd_series: RBD):
     assert rbd_series.G.nodes[5]["k"] == 1
 
 
-def test_rbd_koon_k_given(rbd_koon1: RBD):
-    assert rbd_koon1.G.nodes["source"]["k"] == 1
-    assert rbd_koon1.G.nodes["pump1"]["k"] == 1
-    assert rbd_koon1.G.nodes["pump2"]["k"] == 1
-    assert rbd_koon1.G.nodes["valve"]["k"] == 2
-    assert rbd_koon1.G.nodes["sink"]["k"] == 1
+def test_rbd_koon_k_given(rbd1_koon: RBD):
+    assert rbd1_koon.G.nodes["source"]["k"] == 1
+    assert rbd1_koon.G.nodes["pump1"]["k"] == 1
+    assert rbd1_koon.G.nodes["pump2"]["k"] == 1
+    assert rbd1_koon.G.nodes["valve"]["k"] == 2
+    assert rbd1_koon.G.nodes["sink"]["k"] == 1
+
+
+def test_rbd_get_min_path_sets_rbd_series_koon(rbd_series_koon: RBD):
+    assert {} == rbd_series_koon.get_min_path_sets()
+
+
+def test_rbd_get_min_path_sets_rbd1_koon(rbd1_koon: RBD):
+    assert {
+        frozenset(["source", "pump1", "pump2", "valve", "sink"])
+    } == rbd1_koon.get_min_path_sets()
+
+
+def test_rbd_get_min_path_sets_rbd1_exclude_in_out_node_koon(rbd1_koon: RBD):
+    assert {
+        frozenset(["pump1", "pump2", "valve"])
+    } == rbd1_koon.get_min_path_sets(include_in_out_nodes=False)
+
+
+def test_rbd_get_min_path_sets_rbd2_koon(rbd2_koon: RBD):
+    assert {
+        frozenset(frozenset([1, 2, 3, 4, 5, 6, 7, 8])),
+    } == rbd2_koon.get_min_path_sets()
+
+
+def test_rbd_get_min_path_sets_rbd3_koon1(rbd3_koon1: RBD):
+    assert {
+        frozenset([0, 1, 2, 6]),
+        frozenset([0, 1, 4, 6]),
+    } == rbd3_koon1.get_min_path_sets()
+
+
+def test_rbd_get_min_path_sets_rbd3_koon2(rbd3_koon2: RBD):
+    assert {frozenset([0, 1, 2, 5, 6])} == rbd3_koon2.get_min_path_sets()
+
+
+def test_rbd_get_min_path_sets_rbd3_koon3(rbd3_koon3: RBD):
+    assert {frozenset([0, 1, 2, 3, 5, 6])} == rbd3_koon3.get_min_path_sets()
