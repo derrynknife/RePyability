504211b66f2f93fa52d8a67518a80ff841835857
derrynknife <derryn.knife@gmail.com>
2022-11-16 20:21:07 +1000

    put example from jupyter notebook into test script as fixture

---
+++ repyability/tests/test_repairable_rbd.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pytest
+import surpyval as surv
+
+from repyability.rbd.repairable_rbd import RepairableRBD
+
+# Test RBDs as pytest fixtures
+@pytest.fixture
+def repairable_rbd1() -> RepairableRBD:
+    """A simple RepairableRBD with three intermediate nodes."""
+    nodes = {
+        "source": "input_node",
+        "pump1": "pump1",
+        "pump2": "pump2",
+        "valve": "valve",
+        "sink": "output_node",
+    }
+    edges = [
+        ("source", "pump1"),
+        ("source", "pump2"),
+        ("pump1", "valve"),
+        ("pump2", "valve"),
+        ("valve", "sink"),
+    ]
+    reliability = {
+        "pump1": surv.Weibull.from_params([30, 2]),
+        "pump2": surv.Weibull.from_params([21, 3.]),
+        "valve": surv.Weibull.from_params([25, 2.7])
+    }
+
+    repair = {
+        "pump1": surv.LogNormal.from_params([0.1, .2]),
+        "pump2": surv.LogNormal.from_params([0.1, .3]),
+        "valve": surv.LogNormal.from_params([0.2, .2])
+    }
+    return RepairableRBD(nodes, reliability, repair, edges)
+
