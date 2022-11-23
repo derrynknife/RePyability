601b7fff6fa10393c48a4e19edb1c2c301c381b8
Anthony Carbone <anthonydavidcarbone@gmail.com>
2022-11-22 16:09:31 -0800

    Added first repairable availability test

--- repyability/tests/test_repairable_rbd.py
+++ repyability/tests/test_repairable_rbd.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 import surpyval as surv
 
@@ -56,3 +57,76 @@ def test_repairable_rbd_missing_repairability_component():
     repairability = {}
     with pytest.raises(ValueError):
         RepairableRBD(nodes, reliability, repairability, edges)
+
+
+# TODO: Parameterise the distribution parameters 2/3 times
+def test_repairable_rbd_availability_1N():
+    """
+    The goal here is to, for only one simulation, test if the availability
+    is being calculated correctly. We're achieving this by seeding the numpy
+    random number generator. NOTE: This of course means that this test is
+    coupled to surpyval's .random() implementation!
+    """
+    # Make a Repairable RBD which has only one non-input/output component
+    # This component has an exponential distribution for both it's
+    # reliability and repairability
+    reliability_dist = surv.Exponential.from_params([0.25])
+    repairability_dist = surv.Exponential.from_params([0.5])
+
+    rbd = RepairableRBD(
+        nodes={
+            "s": "input_node",
+            "c": "c",
+            "t": "output_node",
+        },
+        edges=[
+            ("s", "c"),
+            ("c", "t"),
+        ],
+        reliability={"c": reliability_dist},
+        repairability={"c": repairability_dist},
+    )
+
+    # Before anything, set the numpy seed
+    np.random.seed(1)
+
+    # Let's run the sim for 50 units of time
+    t_simulation = 50
+
+    # Form expected availability
+    # At t=0, availability=1 (it starts working)
+    exp_t = [0]
+    exp_availability = [1]
+
+    # Get first failure event
+    t_next_event = reliability_dist.random(1)[0]
+    is_comp_working = True
+
+    while t_next_event < t_simulation:
+        if is_comp_working:
+            # Failure event to be applied
+            exp_t.append(t_next_event)
+            exp_availability.append(0)
+            is_comp_working = False
+
+            # And get the next repair event
+            # (the current time + the next event random sample)
+            t_next_event += repairability_dist.random(1)[0]
+
+        else:
+            # Repair event to be applied
+            exp_t.append(t_next_event)
+            exp_availability.append(1)
+            is_comp_working = True
+
+            # And get the next failure event
+            t_next_event += reliability_dist.random(1)[0]
+
+    # Now check if expected == actual, remembering to reset the seed
+    np.random.seed(1)
+    actual_t, actual_availability = rbd.availability(
+        t_simulation=t_simulation, N=1
+    )
+
+    assert pytest.approx(exp_t) == actual_t
+    assert pytest.approx(exp_availability) == actual_availability
