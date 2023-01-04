72e26becdda2ae897b3b2c21ded8335c7dd74b94
derrynknife <derryn.knife@gmail.com>
2023-01-04 12:33:46 +1000

    added lower bound addition. Has great properties for extrapolation. Also format changes

--- repyability/rbd/standby_node.py
+++ repyability/rbd/standby_node.py
@@ -5,7 +5,12 @@ from surpyval import KaplanMeier
 
 
 class StandbyModel:
-    def __init__(self, reliabilities, k=1, n_sims=10_000):
+    def __init__(self, reliabilities, k=1, n_sims=10_000, lower=-np.inf):
+        if k > len(reliabilities):
+            raise ValueError(
+                "Must be more nodes in the standby arrangement"
+                + " than are required (k)"
+            )
         self.reliabilities = reliabilities
         self.k = k
         self.N = len(reliabilities)
@@ -24,8 +29,18 @@ class StandbyModel:
             # If k are required to continue then the sim needs
             # a little more complexity. An individual run instance
             # can be simulated by getting failures for the first k
-            # components. Then each next random failure is then
-            # added to the current smallest of the k 'active'.
+            # components. The simulation then tracks the k active
+            # components. This is done by adding the next random
+            # instance to the lowest of the k active components.
+            # This is because the next standby component will start
+            # working once the next of the k fails. This means the
+            # lowest of the k active components will be the next
+            # to fail. By simply adding the standby failures to the
+            # lowest of the k active, at the end of the simulation
+            # the lowest value in the queue will be the standby nodes
+            # failure time. This simulation is repeated n_sims
+            # number of times and then the model is approximated
+            # with a non parametric estimate.
             x_random = np.zeros(n_sims)
             for i in range(n_sims):
                 pq = PriorityQueue()
@@ -42,11 +57,11 @@ class StandbyModel:
                 x_random[i] = pq.get()
 
         # Finish by creating the approximation of the
-        # standby arrangement
-        self.model = KaplanMeier.fit(x_random)
+        # standby arrangement.
+        self.model = KaplanMeier.fit(x_random, set_lower_limit=lower)
 
-    def sf(self, x):
-        return self.model.sf(x)
+    def sf(self, *args, **kwargs):
+        return self.model.sf(*args, **kwargs)
 
-    def ff(self, x):
-        return self.model.ff(x)
+    def ff(self, *args, **kwargs):
+        return self.model.ff(*args, **kwargs)
